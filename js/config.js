9eedbde1c87be8a65859482af16d0dae967f495c
pabloheaney <pablo.heaney@go.sfcollege.edu>
2026-08-19 11:35:39 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// GitHub Pages 部署前，如後端網域有變更，只需修改這個檔案。
+window.APP_CONFIG = Object.freeze({
+  API_BASE_URL: "https://api2.atlrunner.dpdns.org",
+  SESSION_DURATION_MS: 15 * 60 * 1000,
+  PENDING_LOGIN_DURATION_MS: 10 * 60 * 1000,
+  TASK_POLL_INTERVAL_MS: 2500
+});
